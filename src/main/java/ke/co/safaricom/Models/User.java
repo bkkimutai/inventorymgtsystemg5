40cbe3a447733905361f47a8a5de6807cfd6dc2c
bkkimutai <kbmsama@gmail.com>
2023-10-23 12:09:41 +0300

    users model

---
+++ src/main/java/ke/co/safaricom/Models/User.java
@@ -0,0 +1,75 @@
+package ke.co.safaricom.Models;
+
+import ke.co.safaricom.DB.DB;
+import ke.co.safaricom.DB.DBManagement;
+import org.sql2o.Connection;
+
+public class User implements DBManagement {
+    private String name;
+    private String email;
+    String userType;
+    private int userId;
+
+
+    public User(String name, String email, String userType) {
+        this.name = name;
+        this.email = email;
+        this.userType = userType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public boolean equals(Object otherUser) {
+        if (!(otherUser instanceof User)) {
+            return false;
+        } else {
+            User newUser = (User) otherUser;
+            return this.getName().equals(newUser.getName()) &&
+                    this.getEmail().equals(newUser.getEmail());
+        }
+    }
+    @Override
+    public void save() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO users (name, email, userType) VALUES (:name, :email, :userType)";
+            this.userId = (int) con.createQuery(sql, true)
+                    .addParameter("name", this.name)
+                    .addParameter("email", this.email)
+                    .addParameter("userType", this.userType)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+
+}
